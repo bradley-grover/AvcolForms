3ef6623cc6316641ac13ae3ea48a12dbdc9fbaa8
Bradley Grover <ac111897@avcol.school.nz>
2022-04-12 20:29:32 +1200

    add base classes and the library for the email client

---
+++ AvcolForms.Core/Email/EmailClient.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Licensed under the MIT License
+ * Copyright (c) 2022 Bradley Grover
+ */
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace AvcolForms.Core.Email;
+
+/// <summary>
+/// Default client implementation of <see cref="IEmailClient"/>
+/// </summary>
+public class EmailClient : IEmailClient
+{
+    
+}
---
+++ AvcolForms.Core/Email/IEmailClient.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Licensed under the MIT License
+ * Copyright (c) 2022 Bradley Grover
+ */
+
+namespace AvcolForms.Core.Email;
+
+/// <summary>
+/// Abstraction for email commuication
+/// </summary>
+public interface IEmailClient
+{
+}
