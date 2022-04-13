d5f9686e5741f6c64d792f7bdbbdb5dd454c2699
Bradley Grover <ac111897@avcol.school.nz>
2022-04-14 11:02:49 +1200

    work for organizing unit tests in the project by covering the overloads of it

---
+++ AvcolForms.Tests/Unit/Core/Files/SaveOnStream.cs
@@ -0,0 +1,15 @@
+﻿using AvcolForms.Core.FileSaving;
+
+namespace AvcolForms.Tests.Unit.Core.Files;
+
+/// <summary>
+/// Tests an overload of <see cref="IFileSaver.SaveAsync(Stream, string, CancellationToken)"/>
+/// </summary>
+public class SaveOnStreamTests
+{
+    private static readonly IFileSaver _fileSaver = new FileSaver();
+    private readonly string _filePath = Path.Join(Directory.GetCurrentDirectory(), "/Assets/savedFile.png");
+
+
+
+}
