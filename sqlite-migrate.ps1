0371e0b8e48bb9b84281d12d1d95db1a1eb3d9b3
Bradley Grover <ac111897@avcol.school.nz>
2022-05-23 17:39:37 +1200

    finish powershell scripts for current supported providers

--- sqlite-migrate.ps1
+++ sqlite-migrate.ps1
@@ -1,4 +1,3 @@
 ﻿$migrationName=$args[0];
-dotnet tool install --global dotnet-ef
 
 dotnet-ef migrations add $migrationName --context ApplicationDbContext --startup-project AvcolForms.Web --project AvcolForms.Core.Data.Sqlite --configuration release
