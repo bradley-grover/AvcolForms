04b00b440f012973816002d429b8e085d1d44da0
Bradley Grover <ac111897@avcol.school.nz>
2022-05-23 14:52:05 +1200

    try merge back into master

--- sqlite-migrate.ps1
+++ sqlite-migrate.ps1
@@ -1,3 +1,4 @@
 ﻿$migrationName=$args[0];
+dotnet tool install --global dotnet-ef
 
 dotnet-ef migrations add $migrationName --context ApplicationDbContext --startup-project AvcolForms.Web --project AvcolForms.Core.Data.Sqlite --configuration release
