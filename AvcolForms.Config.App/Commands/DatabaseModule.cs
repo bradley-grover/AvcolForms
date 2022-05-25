e352167bf2bfc227734171ab27a67520050e4d9c
Bradley Grover <ac111897@avcol.school.nz>
2022-05-25 12:40:54 +1200

    work on database migration from the console

--- AvcolForms.Config.App/Commands/DatabaseModule.cs
+++ AvcolForms.Config.App/Commands/DatabaseModule.cs
@@ -1,4 +1,6 @@
 ﻿using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
 using System.Text.Json;
 
 namespace AvcolForms.Config.App.Commands;
@@ -19,13 +21,29 @@ public class DatabaseModule
             return;
         }
 
-        using var ps = PowerShell.Create();
+        var iss = InitialSessionState.CreateDefault2();
 
-        ps.AddScript(Path.Join(result, "sqlite-migrate.ps1"))
+        iss.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Bypass;
+
+        using var ps = PowerShell.Create(iss);
+
+        string path = Path.Join(result, "sqlite-migrate.ps1");
+
+        ps.AddScript(path)
             .AddArgument(parameters.FirstOrDefault());
 
 
+
         var psResults = await ps.InvokeAsync();
+
+
+        var stringBuilder = new StringBuilder();
+
+
+        foreach (PSObject obj in psResults)
+        {
+            Console.WriteLine(obj.ToString());
+        }
 
         if (ps.HadErrors)
         {
