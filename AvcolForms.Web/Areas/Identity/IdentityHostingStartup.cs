821bea22964f1b8020fd9cbe8f4dcf8ace8261ba
Bradley Grover <ac111897@avcol.school.nz>
2022-06-01 12:28:42 +1200

    add scaffolded items for the project - this in in razor .cshtml, we will change this later

---
+++ AvcolForms.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,11 @@
+﻿[assembly: HostingStartup(typeof(AvcolForms.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AvcolForms.Web.Areas.Identity;
+
+public class IdentityHostingStartup : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices((context, services) => {
+        });
+    }
+}
