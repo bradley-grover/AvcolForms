196a522c9171bc38fca5fbf16b77d703afb0f6c7
Bradley Grover <ac111897@avcol.school.nz>
2022-06-15 11:50:31 +1200

    push account settings

---
+++ AvcolForms.Core/Accounts/AccountSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvcolForms.Core.Accounts;
+
+/// <summary>
+/// Account settings for the application
+/// </summary>
+public class AccountSettings
+{
+    /// <summary>
+    /// The length that a password must be for an account
+    /// </summary>
+    public int PasswordLength { get; set; }
+}
---
+++ AvcolForms.Core/Accounts/SudoSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvcolForms.Core.Accounts;
+
+#nullable disable
+
+/// <summary>
+/// Settings to add a admin user to the debug database for easier testing
+/// </summary>
+public class SudoSettings
+{
+    /// <summary>
+    /// The email for the account
+    /// </summary>
+    [EmailAddress]
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Password to sign up the account
+    /// </summary>
+    public string Password { get; set; }
+}
