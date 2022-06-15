fe23c331f05b077b40a2e495d5e2fb524d7d41e9
Bradley Grover <ac111897@avcol.school.nz>
2022-06-15 22:08:31 +1200

    work on config app for future setup

---
+++ AvcolForms.Config.App/Commands/HelpModule.cs
@@ -0,0 +1,11 @@
+﻿namespace AvcolForms.Config.App.Commands;
+
+[Module("help", "lists info about other commands")]
+public class HelpModule
+{
+    [Command("help", "lists all the available commands")]
+    public async Task HelpAsync()
+    {
+
+    }
+}
