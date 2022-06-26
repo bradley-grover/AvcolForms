ab14dbdeab156942ad0e1a906f8ac7ce0ca64947
Bradley Grover <ac111897@avcol.school.nz>
2022-06-26 18:56:18 +1200

    update options validations a bit

--- AvcolForms.Core/Options/EmailOptions.cs
+++ AvcolForms.Core/Options/EmailOptions.cs
@@ -13,31 +13,37 @@ public class EmailOptions
     /// The email address that sends the email
     /// </summary>
     [EmailAddress]
+    [Required]
     public string FromEmail { get; set; }
 
     /// <summary>
     /// The network port that the server connects to
     /// </summary>
     [Range(0, ushort.MaxValue)]
+    [Required]
     public int Port { get; set; }
 
     /// <summary>
     /// The name of the sender
     /// </summary>
+    [Required]
     public string SenderName { get; set; }
 
     /// <summary>
     /// The smtp server being used for the connection
     /// </summary>
+    [Required]
     public string SmtpServer { get; set; }
 
     /// <summary>
     /// The smtp user name
     /// </summary>
+    [Required]
     public string UserName { get; set; }
 
     /// <summary>
     /// The password for smtp authentication
     /// </summary>
+    [Required]
     public string Password { get; set; }
 }
