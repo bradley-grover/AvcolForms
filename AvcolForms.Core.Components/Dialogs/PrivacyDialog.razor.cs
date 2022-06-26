51da9aaaa0380bdc71e6d42d54c9f3b1e97430b7
Bradley Grover <ac111897@avcol.school.nz>
2022-06-26 20:03:36 +1200

    tidy components library by splitting code and markup

---
+++ AvcolForms.Core.Components/Dialogs/PrivacyDialog.razor.cs
@@ -0,0 +1,45 @@
+﻿using AvcolForms.Core.Privacy;
+
+namespace AvcolForms.Core.Components.Dialogs;
+
+/// <summary>
+/// Privacy dialog for the user to agree to
+/// </summary>
+public partial class PrivacyDialog
+{
+#nullable disable
+    [CascadingParameter] 
+    MudDialogInstance MudDialog { get; set; }
+
+    [Inject] 
+    IPrivacyRetriever Privacy { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        Loading = true;
+
+        Content = await Privacy.RetrieveAsync();
+
+        Loading = false;
+    }
+
+    /// <summary>
+    /// Content to display
+    /// </summary>
+    private string Content;
+
+    /// <summary>
+    /// Flag for when the content is getting loaded
+    /// </summary>
+    private bool Loading = false;
+
+    /// <summary>
+    /// Closes the privacy dialog
+    /// </summary>
+    private void Ok()
+    {
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+}
