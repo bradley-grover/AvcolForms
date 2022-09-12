b66d3438a8d40087451f3229925675f41649bf11
Bradley Grover <ac111897@avcol.school.nz>
2022-09-13 11:04:08 +1200

    begin setup of table for users

---
+++ AvcolForms.Web/Areas/Account/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AvcolForms.Web.Areas.Account.ViewModels;
+
+#nullable disable
+
+public class UserViewModel
+{
+    public string Email { get; set; }
+    public bool EmailIsConfirmed { get; set; }
+}
--- AvcolForms.Web/Areas/Admin/Users.razor.cs
+++ AvcolForms.Web/Areas/Admin/Users.razor.cs
@@ -1,14 +1,48 @@
-﻿namespace AvcolForms.Web.Areas.Admin;
+﻿using AvcolForms.Web.Areas.Account.ViewModels;
+
+namespace AvcolForms.Web.Areas.Admin;
 
 /// <summary>
 /// Page to manage users in administrative mode
 /// </summary>
 public partial class Users
 {
+#nullable disable
+    private MudTable<UserViewModel> userTable;
+    private readonly List<int> _items = new();
+    private string searchString;
+    private int pageSize = 10;
+
+    [Inject]
+    private IDbContextFactory<ApplicationDbContext> Factory { get; set; }
+
+    private bool Lock { get; set; } = false;
+#nullable restore
+
     private readonly List<BreadcrumbItem> items = new()
     {
         new("Home", href: Routes.Home, disabled: false, Icons.Material.Filled.Home),
         new("Admin", href: Routes.Admin.Dash, disabled: false, icon: Icons.Material.Filled.AdminPanelSettings),
         new("Manage Users", href: null, disabled: true, icon: Icons.Material.Filled.ManageAccounts)
     };
+
+    private async Task<TableData<UserViewModel>> GetUsersAsync(TableState state)
+    {
+        using var context = Factory.CreateDbContext();
+
+        var itemCount = await context.Users.CountAsync();
+
+        var items = await context.Users.Take(pageSize).ToListAsync();
+
+        var data = items.Select(x =>
+        {
+            return new UserViewModel()
+            {
+                Email = x.Email,
+                EmailIsConfirmed = x.EmailConfirmed
+            };
+        });
+
+        return new TableData<UserViewModel> { Items = data, TotalItems = itemCount };
+    }
 }
