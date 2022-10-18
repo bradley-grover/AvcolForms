dfe53263aa0e02bbbf6074246caeb6f08993d555
Bradley Grover <ac111897@avcol.school.nz>
2022-10-18 14:07:33 +1300

    add DbRecord - a generic base class

---
+++ AvcolForms.Core.Data/Models/DbRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace AvcolForms.Core.Data.Models;
+
+/// <summary>
+/// Represents an abstract database record which contains useful metadata on the database record inherited from
+/// </summary>
+public abstract class DbRecord
+{
+    /// <summary>
+    /// Represents the date that the record in the database was inserted, which contains an offset as well as the date
+    /// </summary>
+    [Required]
+    [Display(Name = "Date Created")]
+    [JsonPropertyName("created")]
+    public DateTimeOffset Created { get; set; }
+
+    /// <summary>
+    /// Represents the date that the database record was last modified by
+    /// </summary>
+    [Required]
+    [Display(Name = "Date Created")]
+    [JsonPropertyName("modified")]
+    public DateTimeOffset Modified { get; set; }
+}
